a94b48c4b7eb21ae652a0144fb6f4738a3ff176f
Jeeyoung <64334381+hancandice@users.noreply.github.com>
2023-01-05 15:47:15 +0100

    add socket io

--- socket.js
+++ socket.js
@@ -1,26 +1,26 @@
-const WebSocket = require("ws");
+const SocketIO = require("socket.io");
 
 module.exports = (server) => {
-  const wss = new WebSocket.Server({ server });
+  const io = SocketIO(server, { path: "./socket.io" });
 
-  wss.on("connection", (ws, req) => {
-    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    console.log("New client connected: ", ip);
-    ws.on("message", (message) => {
-      console.log(message.toString());
+  io.on("connection", (socket) => {
+    const req = socket.request;
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+    console.log("New client connected: ", ip, socket.id, req.ip);
+
+    socket.on("disconnect", () => {
+      console.log("Client disconnected: ", ip, socket.id);
+      clearInterval(socket.interval);
     });
-    ws.on("error", (error) => {
+    socket.on("error", (error) => {
       console.error(error);
     });
-    ws.on("close", () => {
-      console.log("Client disconnected: ", ip);
-      clearInterval(ws.interval);
+    socket.on("reply", (data) => {
+      console.log(data);
     });
-
-    ws.interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send("Send message from server to client");
-      }
+    socket.interval = setInterval(() => {
+      socket.emit("news", "Hello Socket.IO");
     }, 3000);
   });
 };
